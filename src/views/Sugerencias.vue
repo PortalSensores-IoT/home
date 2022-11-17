5f8229dbef4364e5549bbc9883ebd77c3bceba11
Lautaro Gimenez <ldg.gimenez98@gmail.com>
2022-11-16 23:30:45 -0300

    tabs sugerencias

--- src/views/Sugerencias.vue
+++ src/views/Sugerencias.vue
@@ -1,9 +1,135 @@
 <template>
-  <div>
+  <!--<div>
     <button id="btnEnviarSugerencia" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalAltaSugerencia" v-show="this.autorizaciones['Alta sugerencia']">
       Enviar sugerencia
     </button>
+  </div>-->
+
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button class="nav-link active" id="cardSugerenciasPendientes-tab" data-bs-toggle="tab" data-bs-target="#cardSugerenciasPendientes" type="button" role="tab" aria-controls="cardSugerenciasPendientes" aria-selected="true">Pendientes</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="cardSugerenciasRechazadas-tab" data-bs-toggle="tab" data-bs-target="#cardSugerenciasRechazadas" type="button" role="tab" aria-controls="cardSugerenciasRechazadas" aria-selected="false">Aprobadas</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="cardSugerenciasAprobadas-tab" data-bs-toggle="tab" data-bs-target="#cardSugerenciasAprobadas" type="button" role="tab" aria-controls="cardSugerenciasAprobadas" aria-selected="false">Rechazadas</button>
+    </li>
+  </ul>
+
+  <div class="tab-content" id="myTabContent">
+    <div id="cardSugerenciasPendientes" class="card tab-pane fade show active" role="tabpanel" aria-labelledby="cardSugerenciasPendientes-tab">
+      <div id="cardSugerenciasPendientesHeader" class="card-header py-3">
+        <b>Sugerencias pendientes</b>
+      </div>
+      <div class="card-body">
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Tipo de sugerencia</th>
+              <th scope="col">Tipo de sensor</th>
+              <th scope="col">Descripción</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div id="cardSugerenciasRechazadas" class="card tab-pane fade" role="tabpanel" aria-labelledby="cardSugerenciasRechazadas-tab">
+      <div id="cardSugerenciasPendientesHeader" class="card-header py-3">
+        <b>Sugerencias rechazadas</b>
+      </div>
+      <div class="card-body">
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Tipo de sugerencia</th>
+              <th scope="col">Tipo de sensor</th>
+              <th scope="col">Descripción</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div id="cardSugerenciasAprobadas" class="card tab-pane fade" role="tabpanel" aria-labelledby="cardSugerenciasAprobadas-tab">
+      <div id="cardSugerenciasPendientesHeader" class="card-header py-3">
+        <b>Sugerencias aprobadas</b>
+      </div>
+      <div class="card-body">
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Tipo de sugerencia</th>
+              <th scope="col">Tipo de sensor</th>
+              <th scope="col">Descripción</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
   </div>
+
   <FormAltaSugerencia/>
 
 </template>
@@ -55,6 +181,20 @@ export default {
 
 
 <style scoped>
+
+  #cardSugerenciasPendientes {
+    border-radius: 0% !important;
+  }
+
+  #cardSugerenciasPendientesHeader {
+    
+  }
+
+
+
+
+
+
   #cardSugerencias {
     padding: 0;
     background-color: #DADED3;
